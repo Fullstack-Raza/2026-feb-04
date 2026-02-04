241cbf64263b85af84c31b8787c92f22775f1342
Fullstack-Raza <168615664+Fullstack-Raza@users.noreply.github.com>
2026-02-04 22:23:06 +0300

    solve task 3

--- js.js
+++ js.js
@@ -19,5 +19,61 @@ btn.addEventListener("click", function () {
   let green = Math.floor(Math.random() * 256);
   let blue = Math.floor(Math.random() * 256);
   box.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-//   console.log(red, green, blue);
+  //   console.log(red, green, blue);
 });
+
+// task 1 multi-task loop
+
+// let numbers = [2, 5, 8, 5, 3, 10, 2, 8, 11, 14];
+// let unique = [];
+// let even = [];
+// let evenSum = 0;
+// let totlSum = 0;
+// let max = numbers[0];
+// for (let i = 0; i < numbers.length; i++) {
+//   totlSum = totlSum + numbers[i];
+//   if (max < numbers[i]) {
+//     max = numbers[i];
+//   }
+//   if (!unique.includes(numbers[i])) {
+//     unique.push(numbers[i]);
+//   }
+//   if (numbers[i] % 2 === 0) {
+//     even.push(numbers[i]);
+//     evenSum = evenSum + numbers[i];
+//   }
+// }
+// console.log(unique);
+// console.log(even);
+// console.log(evenSum);
+// console.log(totlSum);
+// console.log(max);
+
+// task 2
+// let numbers = [2, 10, 5, 10, 3];
+// let max = 0;
+// let list=0;
+// for (let i = 0; i < numbers.length; i++) {
+//   if (max < numbers[i]) {
+//    max = numbers[i]
+//   }
+//   if(max === numbers[i]){
+//     list++
+//   }
+// }
+//   console.log(max,list);
+
+// task 3
+let numbers = [3, 7, 2, 9, 5];
+let max = numbers[0];
+let secondMax = numbers[0];
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > max) {
+    secondMax = max;
+    max = numbers[i];
+  } else if (numbers[i] > secondMax) {
+    secondMax = numbers[i];
+  }
+}
+console.log(max);
+console.log(secondMax);
